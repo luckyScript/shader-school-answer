b68cf6cec361bc1e4cbb7a5b71bb97c3ff681495
luckyscript <jsjhlk@qq.com>
2017-11-26 15:37:45 +0800

    05

--- 05-intro-5/mandelbrot.glsl
+++ 05-intro-5/mandelbrot.glsl
@@ -1,9 +1,21 @@
+vec2 generatemandel(vec2 z, vec2 c)
+{
+  return vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+}
 bool mandelbrot(highp vec2 c) {
 
   //Test if the point c is inside the mandelbrot set after 100 iterations
+  bool isIn = false;
   vec2 z = vec2(0.0);
-
-  return false;
+  for (int i = 0; i != 100; i++)
+  {
+    z = generatemandel(z, c);
+  }
+  if (length(z) < 2.0)
+  {
+    isIn = true;
+  }
+  return isIn;
 }
 
 
